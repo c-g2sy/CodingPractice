31360cf73c448bc21e7b91904b75dc9af9a6c121
c-g2sy <cg2sy.prog@gmail.com>
2022-09-08 13:46:20 +0000

    FreeCodeCamp

---
+++ portScanner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Import module
+import socket
+
+# Shorten the variables
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Don't wait forever!
+s.settimeout(10)
+
+# Hard code or ask for user input
+host = input("Enter the target IP address:  ")
+port = int(input("Enter first port to scan:  "))
+
+# Define boolean results
+def portScanner(port):
+    if s.connect_ex((host, port)):
+        print("Port is closed! :(")
+    else:
+        print("Port is open!")
+
+portScanner(port)
