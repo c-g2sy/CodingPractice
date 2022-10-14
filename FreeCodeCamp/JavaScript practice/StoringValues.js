66c0a12daeda62686a588e0cff9b084d263aecc4
c-g2sy <cg2sy.prog@gmail.com>
2022-10-14 14:22:11 +0000

    Scratch pad for the JS practice on FreeCodeCamp

---
+++ FreeCodeCamp/JavaScript practice/StoringValues.js
@@ -0,0 +1,16 @@
+// Declaration and operations
+var a;
+a = 7;
+var b;
+b = a;
+
+// Declaration and assignment in one line
+var myFirstName = 'cg2sy';
+var myLastName = 'Loki';
+
+// Delclaration using 'let' instead of 'var' prevents variable overwrite
+let catName = "Meow";
+let catSound = "Purrr";
+// Use const for read-only declarations
+const catColor = "Black";
+
