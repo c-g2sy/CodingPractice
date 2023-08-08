a4b4059832a5236de0535dcc8b1385005abda3b5
c-g2sy <cg2sy.prog@gmail.com>
2023-08-08 17:47:59 +0000

    Added error handling to try and find correct count

--- base64_decode.sh
+++ base64_decode.sh
@@ -13,8 +13,15 @@ encoded_string="$2"
 # Loop 'x' times and print decoded string after each iteration
 for ((i=0; i<x; i++)); do
     decoded_string="$(echo "$encoded_string" | base64 -d)"
+
+    # Check if base64 decoding was successful
+    if [ $? -ne 0 ]; then
+        echo "String fully decoded after $i iterations"
+        exit 1
+    fi
+
     encoded_string="$decoded_string"
-    echo "$decoded_string"
+    echo "$i: "$decoded_string""
 done
 
 # Print final decoded string after 'x' iterations
