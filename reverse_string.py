ab9cc378f245dbca420101b343b7d7e48b938c8f
c-g2sy <cg2sy.prog@gmail.com>
2023-08-08 19:54:30 +0000

    Move variables and print outside logic block

--- reverse_string.py
+++ reverse_string.py
@@ -6,10 +6,9 @@ print_reverse = lambda s: s[::-1]
 if len(sys.argv) > 1:
     # If a command-line argument is provided, use it as input
     input_string = ' '.join(sys.argv[1:])
-    reversed_string = print_reverse(input_string)
-    print("Reversed string:", reversed_string)
 else:
     # If no command-line argument, prompt the user for input
     user_input = input("Enter a string: ")
-    reversed_string = print_reverse(user_input)
-    print("Reversed string:", reversed_string)
+
+reversed_string = print_reverse(input_string)
+print("Reversed string:", reversed_string)
