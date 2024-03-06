49b0871b4fddcb24e228ef4e9953d7c0d711d74d
c-g2sy <cg2sy.prog@gmail.com>
2024-03-06 17:21:09 -0500

    Created weak_rsa.py

---
+++ HTB_Main/weak_rsa.py
@@ -0,0 +1,10 @@
+from Crypto.PublicKey import RSA
+
+def get_pubkey(f):
+    with open(f) as pub:
+        key = RSA.importKey(pub.read())
+    return (key.n, key.e)
+
+N, e = get_pubkey('./key.pub')
+
+print(f'{e = }')
