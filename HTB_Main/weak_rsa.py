a55be613e266fccc2ad2e75f77c920c280e33b08
c-g2sy <cg2sy.prog@gmail.com>
2024-03-06 17:50:29 -0500

    Added owiener

--- HTB_Main/weak_rsa.py
+++ HTB_Main/weak_rsa.py
@@ -1,4 +1,5 @@
 from Crypto.PublicKey import RSA
+import owiener
 
 def get_pubkey(f):
     with open(f) as pub:
@@ -7,5 +8,26 @@ def get_pubkey(f):
 
 N, e = get_pubkey('./key.pub')
 
+def get_ciphertext(f):
+    with open(f, 'rb') as ct:
+        return bytes_to_long(ct.read())
+
+d = owiener.attack(e, N)
+
+def decrypt_rsa(N, e, d, ct):
+    pt = pow(ct, d, N)
+    return long_to_bytes(pt)
+
 print(f'{e = }')
 print(f'{N = }')
+
+def pwn():
+    N, e = get_pubkey('./key.pub')
+    ct = get_ciphertext('./flag.enc')
+    d = owiener.attack(e, N)
+flag = decrypt_rsa(N, e, d, ct)
+
+print(flag)
+
+if __name__ == '__main__':
+    pwn()
